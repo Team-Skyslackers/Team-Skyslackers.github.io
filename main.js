9306333df8b797d8e3f4a156bace92da7be71e9f
Zhang Haoyu <haoyusimon927@gmail.com>
2021-07-10 18:01:21 +0800

    Added database login

--- main.js
+++ main.js
@@ -1,3 +1,139 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC4gdo6gFSc_W90EBkoMNieCRqttQZ2zWw",
+  authDomain: "test-7f7c0.firebaseapp.com",
+  projectId: "test-7f7c0",
+  storageBucket: "test-7f7c0.appspot.com",
+  databaseURL: "https://test-7f7c0-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  messagingSenderId: "531047203297",
+  appId: "1:531047203297:web:e005e20157956859bb12a0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var currentUser = {};
+DB = firebase.database();
+
+function getUTCDateAndTime(){
+  // example: 2021-07-03T09:28:49
+  var d = new Date();
+  return d.getUTCFullYear().toString().padStart(4, '0') + '-' + (d.getUTCMonth()+1).toString().padStart(2, '0') + '-' + d.getUTCDate().toString().padStart(2, '0') + 'T' +
+  d.getUTCHours().toString().padStart(2, '0') + ':' + d.getUTCMinutes().toString().padStart(2, '0') + ':' + d.getUTCSeconds().toString().padStart(2, '0');
+}
+
+// Register function
+function RegisterUser(email, password, confirmPassword){
+  if (password != confirmPassword){
+      alert("Confirm password does not match.");
+  } else {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+          // Signed in 
+          var user = userCredential.user;
+
+          console.log("Successfully registered new user");
+          DB.ref('users/'+user.uid).set({
+              userEmail: email,
+              registerDateAndTimeUTC: getUTCDateAndTime()
+          });
+      })
+      .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          alert(errorMessage);
+      });
+  }
+}
+
+// Login function
+function SigninUser(email, password){
+  firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user;
+
+          console.log("Successfully signed in");
+      })
+      .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          alert(errorMessage);
+      });
+}
+
+// Google signin function
+var provider = new firebase.auth.GoogleAuthProvider();
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+function GoogleSignin(){
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+  }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+  });
+}
+
+// Logout function
+function SignOut(){
+  firebase.auth().signOut()
+  .then(() => {
+      console.log("Succesfully signed out");
+  })
+  .catch((error) => {
+      console.log(error.message);
+  });
+}
+
+// Change in authentication state
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      currentUser = user;
+
+      // update user last signin time
+      var d = new Date();
+      DB.ref('users/' + user.uid).update({
+          lastLoginDateAndTimeUTC: getUTCDateAndTime()
+      });
+
+      // ws.send("uid:" + user.uid);
+
+      $("#signin-form").addClass("d-none");
+      $("#registration-form").addClass("d-none");
+      $("#signout-form").removeClass("d-none");
+
+      console.log(user.email + " has signed in");
+      // ...
+  } else {
+      // User is signed out
+      // ...
+      currentUser = {};
+      $("#signin-form").removeClass("d-none");
+      $("#registration-form").addClass("d-none");
+      $("#signout-form").addClass("d-none");
+
+      console.log("No user signed in")
+  }
+});
+
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -25,59 +161,7 @@ let pin_num = 0;
 let leftpx = 74;
 let rightpx = 1354;
 
-//74～1354px
-// Define the tracks that have to be played
-// let track_list = [
-//   {
-//     name: "Night Owl",
-//     artist: "Broke For Free",
-//     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
-//   },
-//   {
-//     name: "Enthusiast",
-//     artist: "Tours",
-//     image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
-//   },
-//   {
-//     name: "Shipping Lanes",
-//     artist: "Chad Crouch",
-//     image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
-//   },
-// ];
-console.log(slider.style)
-function random_bg_color() {
 
-  // Get a number between 64 to 256 (for getting lighter colors)
-  let red = Math.floor(Math.random() * 256) + 64;
-  let green = Math.floor(Math.random() * 256) + 64;
-  let blue = Math.floor(Math.random() * 256) + 64;
-
-  // Construct a color withe the given values
-  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
-
-  // Set the background to that color
-  document.body.style.background = bgColor;
-}
-
-
-function loadTrack(track_index) {
-  clearInterval(updateTimer);
-  resetValues();
-  curr_track.src = track_list[track_index].path;
-  // curr_track.load();
-
-  // track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
-  // track_name.textContent = track_list[track_index].name;
-  // track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
-
-  updateTimer = setInterval(seekUpdate, 10);
-  curr_track.addEventListener("ended", pauseTrack());
-  random_bg_color();
-}
 
 function resetValues() {
   curr_time.textContent = "00:00";
